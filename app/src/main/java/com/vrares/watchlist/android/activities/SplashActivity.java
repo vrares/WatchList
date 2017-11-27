91393543abc467555c2e347c580b8138f90e38cb
Vultur Rares <rares.vultur96@gmail.com>
2017-11-27 18:50:42 +0200

    Fix Splash

--- app/src/main/java/com/vrares/watchlist/android/activities/SplashActivity.java
+++ app/src/main/java/com/vrares/watchlist/android/activities/SplashActivity.java
@@ -1,8 +1,8 @@
 package com.vrares.watchlist.android.activities;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 
@@ -17,15 +17,23 @@ public class SplashActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash);
-        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
-            Intent intent = new Intent(getApplicationContext(), NavigationActivity.class);
-            startActivity(intent);
-            finish();
-        } else {
-            Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
-            startActivity(intent);
-            finish();
-        }
+        
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+                    Intent intent = new Intent(getApplicationContext(), NavigationActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        }, 2000);
+
+
 
     }
 
